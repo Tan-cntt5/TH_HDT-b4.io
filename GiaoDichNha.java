76fd572625ef3f973eea5c84588f19cb4fd35a6f
Tan-cntt5 <125740918+Tan-cntt5@users.noreply.github.com>
2023-05-22 16:32:31 +0700

    Add files via upload

---
+++ GiaoDichNha.java
@@ -0,0 +1,24 @@
+
+package newpackage;
+
+
+class GiaoDichNha extends GiaoDich {
+    private String loaiNha;
+    private String diaChi;
+
+    public GiaoDichNha(String maGd, String ngayGd, double donGia, double dienTich, String loaiNha, String diaChi) {
+        super(maGd, ngayGd, donGia, dienTich);
+        this.loaiNha = loaiNha;
+        this.diaChi = diaChi;
+    }
+
+    @Override
+    public double tinhThanhTien() {
+        if (loaiNha.equals("cao cấp")) {
+            return dienTich * donGia;
+        } else {
+            return dienTich * donGia * 0.9;
+        }
+    }
+}
+
